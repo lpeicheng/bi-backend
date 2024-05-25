2df0a1f9217e2bd4b4c1963cfe2dcb599e107b79
itcast <hello@itcast.cn>
2024-05-25 21:57:16 +0800

    增加分数功能、上传图片、websocket异步提醒功能、更换ai模型、oss对象存储功能

--- src/main/java/com/yupao/config/OSSConfig.java
+++ src/main/java/com/yupao/config/OSSConfig.java
@@ -1,7 +1,7 @@
 package com.yupao.config;
 
 import lombok.Data;
-import lombok.Value;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
